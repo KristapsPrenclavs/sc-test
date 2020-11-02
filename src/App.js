46b2524c6d8b1557e803fc699d28b6bf90899264
santabirzina <44974661+santabirzina@users.noreply.github.com>
2020-11-02 17:04:04 +0200

    still no infinite option

--- src/App.js
+++ src/App.js
@@ -32,7 +32,6 @@ export const App = () => {
   };
 
   const handleJump = (jumpTo) => {
-    console.log(jumpTo);
     const j = parseInt(jumpTo);
     if (j >= 1 && j <= slides.length) {
       setCount(1);
--- src/Slides.js
+++ src/Slides.js
@@ -2,10 +2,11 @@ import React from "react";
 import "./index.css";
 
 export const Slides = (props) => {
-  const slides = [];
+  const showSlides = [];
+  const cpoy = [...props.slides];
 
   for (let i = props.currentSlide; i < props.count + props.currentSlide; i++) {
-    slides.push(props.slides[i]);
+    showSlides.push(props.slides[i]);
   }
 
   // console.log(slides);
@@ -13,7 +14,7 @@ export const Slides = (props) => {
 
   return (
     <div className="slides" style={{ width: props.slides.length * 500 }}>
-      {slides.map((slide) => {
+      {showSlides.map((slide) => {
         return (
           <section className="slide" key={slide}>
             {slide}
